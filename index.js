1c58f412b3f630319d8edca298bc052819cde528
Mihaela <chiculita.mihaela@yahoo.com>
2024-01-25 17:07:40 +0200

    Add function to clean the string value JS receives from input and make sure value is a no. JS will receive a no from input even tho we set input type to number.

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
+// function to clean the string value from input and ensure it's a number
+function cleanInputString(str) {
+    const strArray = str.split(''); 
+    const cleanStrArray = [];
+  
+    for (let i = 0; i < strArray.length; i++) { 
+   // check if character at index i is not a +, -, space and push it to the cleanStrArray 
+   if(!["+", "-", " "].includes(strArray[i])){
+    cleanStrArray.push(strArray[i]);
+  }
+    }
+  }
