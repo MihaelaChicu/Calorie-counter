c25aaa00af394e553819f880c517f900725637d8
Mihaela <chiculita.mihaela@yahoo.com>
2024-01-31 17:42:15 +0200

    Add more functionality for input entries

--- index.js
+++ index.js
@@ -39,5 +39,35 @@ function cleanInputString(str){
    // The dropdown allows the user to select a category for where the entry goes into - entryDropdown
    //get the value of the selected option with the value property
   function addEntry(){
-     const targetId = "#" + entryDropdown.value;
+
+     //target input-container element within the element that has the targetId
+     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`); 
+
+     // number the entries
+     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+
+     //add string variable with labels and input element
+     const HTMLString = `
+      <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+      <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
+      <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+      <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"></input>
+     `;
+
+      // targetInputContainer and preserve input content with insertAdjacentHTML()
+      targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+  }
+  // Add event listener for addEntryButton
+  addEntryButton.addEventListener("click", addEntry);
+
+
+  // function to get the calories counts from the user's entries
+  function getCaloriesFromInputs(list){ //list parameter is going to be a NodeList
+
+    //loop through the NodeList Elements
+    for(let i=0; i < list.length; i++){
+
+    }
+     let calories = 0;
+
   }
